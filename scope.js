da92193472212a1e7a73888ab1ad4372fc344540
Moyuri Akther <akthermoyuri@.com>
2021-02-03 02:25:07 +0600

    upload

---
+++ scope.js
@@ -0,0 +1,15 @@
+let bonus = 2;
+function sum(first, second) {
+    let result = first + second + bonus;
+    if(result > 9){
+        var mood = 'happy';
+        mood = 'fishy';
+        mood = 'filmy';
+        console.log(mood);
+    }
+    var day = 'friday';
+    console.log(day);
+    return result;
+}
+sum(5, 6);
+
