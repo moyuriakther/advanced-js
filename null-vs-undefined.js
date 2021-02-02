176e8a89b770df7641b4f83ea7acec962a4edb86
Moyuri Akther <akthermoyuri@.com>
2021-02-02 19:34:43 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,41 @@
+1.
+let pakhi;  //value define korenai tai undefined
+console.log(pakhi);
+
+2.
+function add(num1, num2){
+    console.log(num1 + num2);
+                    //return korenai tai undefined
+}
+const result = add(12, 15);
+console.log(result);
+
+3.
+function add(num1, num2){
+    console.log(num1 + num2);
+       return            //return likhche but kichu return korenai tai undefined
+}
+const result = add(12, 15);
+console.log(result);
+
+4.
+function add(num1, num2) { //perameter pass korenai tai undefined
+    console.log(num1, num2);
+}
+const result = add(12);
+console.log(result);
+
+5.
+const premik = {name: 'smart dude', phone:'018382672467'}
+console.log(premik.gf);  //jeta nai seta access korte gele undefined
+
+6.
+let fun = undefined;  //veriable value e undefined set korle undefined dekhabe
+console.log(fun);
+
+7.
+let ages = [12, 13, 14];  
+console.log(ages[16]);  //jeta nai seta khujle undefined dekhabe
+
+
+
