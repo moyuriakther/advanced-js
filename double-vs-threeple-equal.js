e34a0adde29015921f5d99f6b501f7921d1a7810
Moyuri Akther <akthermoyuri@.com>
2021-02-02 20:00:38 +0600

    upload

---
+++ double-vs-threeple-equal.js
@@ -0,0 +1,9 @@
+// double equal type check kore na but threeple equal type check kore.
+//double equal sudhu value check kore but threeple equal value er sathe type o check kore.
+const first = 2;
+const second = '2';
+if (first === second) {
+    console.log("condition true");
+} else {
+    console.log("condition false");
+}
