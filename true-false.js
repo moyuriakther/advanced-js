0686afe9f428d60854a8d3d9364e23bad4e4c18e
Moyuri Akther <akthermoyuri@.com>
2021-02-02 16:13:24 +0600

    update

---
+++ true-false.js
@@ -0,0 +1,14 @@
+//falsy
+// 0 , null, true, "" empty string, undefine variable, NaN
+
+//truthy
+// " ", '0', [], false, 
+
+let nam = true;
+console.log(nam);
+if (nam) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
